759afa20f626fc2a19b19d654a4d8ae6f1bae421
Jack Tiebie <jacktiebie45@gmail.com>
2023-02-20 10:04:51 +0100

    Kookworkshop

---
+++ php/workshopsub1.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<Head>
+    <meta charset="utf-8">
+    <title>contactpagina</title>
+    <link rel=stylesheet href="../css/custom.css">
+    <link rel="stylesheet" href="../css/workshopsub.css">
+</Head>
+
+<body>
+    <?php
+    include 'navbar.php';
+    ?>
+
+    <div class="containerSub">
+        <div class="subLeft">
+            <img class="subImage" src="../foto/indonesischRijstafel.jpg" alt="Foto van indonesische rijsttafel">
+        </div>
+        <div class="subRight">
+            <h2 class="workshopH2">Indonesische rijsttafel</h2>
+            <p class="workshopPrijs">$12 Euro/p.p</p>
+            <h3 class="workshopH3">Over de workshop</h3>
+            <p class="workshopInhoud">orem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+                obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+                nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+            </p>
+            <p class="workshopButton">Reserveer hier</p>
+        </div>
+    </div>
+
+
+
+
+    <!-- Footer van de pagina met contactgegevens -->
+    <footer>
+        <?php
+        include 'footer.php'
+        ?>
+
+    </footer>
+</body>
+
+</html>
