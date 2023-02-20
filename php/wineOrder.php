17ea2f9649c308303132e78d7dc2b03f54b0dfd4
Jack Tiebie <jacktiebie45@gmail.com>
2023-02-20 12:59:03 +0100

    jack

--- php/wineOrder.php
+++ php/wineOrder.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/custom.css">
+    <link rel="stylesheet" href="../css/wineOrder.css">
     <title>Document</title>
 </head>
 
@@ -16,20 +17,29 @@
     include 'navbar.php';
     ?>
     <div>
-        <?php
-        //krijg alle items van de drinkcard tabel
-        $sql = "SELECT ID, name, price, inStock FROM drinkcard";
-        $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
-            // output data of each row
-            while ($row = $result->fetch_assoc()) {
-                echo "id: " . $row["ID"] . " inStock: " . $row["inStock"] . " - Name: " . $row["name"] . " " . $row["price"] . "<br>";
+        <div class="wineOrderDiv">
+            <?php
+            //krijg alle items van de drinkcard tabel
+            $sql = "SELECT ID, name, price, inStock FROM drinkcard";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while ($row = $result->fetch_assoc()) {
+            ?>
+                    <div class="wineOrderResult">
+                        <?php
+                        echo "Name: " . $row["name"] . "<br>" . "Price:" . $row["price"] . "<br>"; ?>
+                    </div>
+            <?php
+                }
+            } else {
+                echo "0 results";
             }
-        } else {
-            echo "0 results";
-        }
-        $conn->close();
-        ?>
+            $conn->close();
+            ?>
+
+        </div>
+
 
     </div>
     <?php
