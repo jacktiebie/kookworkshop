cc1a5d5df3fde2575de1fee6440b97860cc81e2d
Jack Tiebie <jacktiebie45@gmail.com>
2023-02-20 13:08:16 +0100

    Update contact.php

--- php/contact.php
+++ php/contact.php
@@ -14,56 +14,69 @@
   include 'navbar.php';
   session_start();
   $uid = $_SESSION['userid'];
+  if (isset($_SESSION["useruid"])) {
   ?>
-  <!-- Inputfields voor de contact pagina -->
-  <div class="container">
-    <div class="contact">
-      <div class="col1">
-        <h1>Stel vragen...</h1>
-        <p>Stuur ons een berichtje met het contact formulier of geef ons een belletje.</p><br>
-        <p></p>
-        <a href="tel:+7094137055">Tel: 7094137055</a>
-      </div>
-      <div class="col2">
-        <h1>Contact formulier</h1>
-        <form action="contact.php" method="POST"><br>
-          <input type="text" name="titel" placeholder="Uw titel!" required><br>
-          <textarea name="bericht" placeholder="Zet hier uw bericht neer!" required></textarea><br>
-          <button type="submit" name="submit">Verzenden</button>
-          <?php
-          $stmt = $conn->prepare("INSERT INTO contact (ID, usersId, titel, bericht) VALUES (null, ?, ?, ?)");
 
-          if (isset($_POST["submit"])) {
-            $titel = $_POST["titel"];
-            $bericht = $_POST["bericht"];
+    <!-- Inputfields voor de contact pagina -->
+    <div class="container">
+      <div class="contact">
+        <div class="col1">
+          <h1>Stel vragen...</h1>
+          <p>Stuur ons een berichtje met het contact formulier of geef ons een belletje.</p><br>
+          <p></p>
+          <a href="tel:+7094137055">Tel: 7094137055</a>
+        </div>
+        <div class="col2">
+          <h1>Contact formulier</h1>
+          <form action="contact.php" method="POST"><br>
+            <input type="text" name="titel" placeholder="Uw titel!" required><br>
+            <textarea name="bericht" placeholder="Zet hier uw bericht neer!" required></textarea><br>
+            <button type="submit" name="submit">Verzenden</button>
+            <?php
+            $stmt = $conn->prepare("INSERT INTO contact (ID, usersId, titel, bericht) VALUES (null, ?, ?, ?)");
 
-            $stmt->bind_param("ss", $titel, $bericht);
+            if (isset($_POST["submit"])) {
+              $titel = $_POST["titel"];
+              $bericht = $_POST["bericht"];
 
-            if (emptyInputContact($uid, $titel, $bericht) !== false) {
-              header("location: contact.php?error=emptyinput");
-              exit();
+              $stmt->bind_param("ss", $titel, $bericht);
+
+              if (emptyInputContact($uid, $titel, $bericht) !== false) {
+                header("location: contact.php?error=emptyinput");
+                exit();
+              }
+
+              createContact($conn, $uid, $titel, $bericht);
+
+              $stmt->execute();
+              header("Location: contact.php");
             }
-
-            createContact($conn, $uid, $titel, $bericht);
-
-            $stmt->execute();
-            header("Location: contact.php");
-          }
-          if (isset($_GET["error"])) {
-            if ($_GET["error"] == "emptyinput") {
-              echo "<p class='errormassage'>Vul al de velden in!</p>";
-            } else if ($_GET["error"] == "stmtfailed") {
-              echo "<p class='errormassage'>Er ging iets verkeerd, probeer opnieuw!</p>";
-            } else if ($_GET["error"] == "none") {
-              echo "<p class='errormassage'>Uw bericht is verzonden!</p>";
+            if (isset($_GET["error"])) {
+              if ($_GET["error"] == "emptyinput") {
+                echo "<p class='errormassage'>Vul al de velden in!</p>";
+              } else if ($_GET["error"] == "stmtfailed") {
+                echo "<p class='errormassage'>Er ging iets verkeerd, probeer opnieuw!</p>";
+              } else if ($_GET["error"] == "none") {
+                echo "<p class='errormassage'>Uw bericht is verzonden!</p>";
+              }
             }
-          }
-          ?>
-        </form>
+            ?>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-  <!-- Footer van de pagina met contactgegevens -->
+    <!-- Footer van de pagina met contactgegevens -->
+
+  <?php
+
+
+  } else {
+  ?>
+    <p>Log eerst in alsutblieft bruh</p>
+  <?php
+  }
+
+  ?>
   <div class="bruhfooter">
     <?php
     include 'footer.php';
