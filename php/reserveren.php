9c703831433be9316d2740485e01f459fba15027
S-Kos <103041224+S-Kos@users.noreply.github.com>
2023-02-23 16:39:17 +0100

    styling

--- php/reserveren.php
+++ php/reserveren.php
@@ -29,32 +29,32 @@
                 <p>
                     <label for="firstName">First Name:</label>
                     <input type="text" name="firstName" id="firstName">
-                </p>
+                </p><br>
                 <p>
                     <label for="lastName">Last Name:</label>
                     <input type="text" name="lastName" id="lastName">
-                </p>
+                </p><br>
                 <p>
                     <label for="Email">Email:</label>
                     <input type="text" name="email" id="Email">
-                </p>
+                </p><br>
                 <p>
                     <label for="Address">Phone:</label>
                     <input type="text" name="phone" id="Address">
-                </p>
+                </p><br>
                 <p>
                     <label for="Date">Date:</label>
                     <input type="date" name="date" id="Date">
-                </p>
+                </p><br>
                 <p>
                     <label for="Ws">Workshops:</label>
                     <input type="text" name="ws" id="Ws" list="workshops">
-                </p>
+                </p><br>
                 <p>
                     <label for="Persons">Persons:</label>
                     <input type="number" name="persons" id="Persons">
-                </p>
-                <input type="submit" value="Submit">
+                </p><br>
+                <input type="submit" class="reservebut" value="Submit">
             </form>
         </div>
 
